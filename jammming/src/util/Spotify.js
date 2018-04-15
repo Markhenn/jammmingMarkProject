6da0833a4a55d9e4e973c24f1130eb7f1c96e652
Mark <markhenningerwork@gmail.com>
2018-04-15 21:23:08 +0200

    add get links to spotify.js

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -3,14 +3,24 @@ Spotify access
 Client ID b301371f68f341c8b50a2262ef34a1a8
 Client Secret 20c80ad5a49644199be078e759650ed7
 
+GET
 Track Search:
 "https://api.spotify.com/v1/search?q=hulapalu&type=track"
 
 Artist Search:
+"https://api.spotify.com/v1/search?q=hulapalu&type=artist"
+
+Artist Tracks:
+https://api.spotify.com/v1/artists/{id}/top-tracks
 
 
 Album Search: 
+"https://api.spotify.com/v1/search?q=hulapalu&type=album"
+
+Album Track Search
+https://api.spotify.com/v1/albums/{id}/tracks
 
 
+POST
 */
 
